58ecce290ce1bcf52ed1ad0403d72732e7034452
asu <arsenyk.ustaris.dev@gmail.com>
2015-09-17 10:23:18 -0700

    Initial Commit

---
+++ Receipts++/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Receipts++
+//
+//  Created by asu on 2015-09-17.
+//  Copyright © 2015 asu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
